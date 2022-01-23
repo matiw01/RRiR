e48ab5f1ab818aaaa1932d6e9137032f6945e21d
Mateusz Wiecek <mateusz.wiecek12345@gmail.com>
2022-01-23 19:44:20 +0100

    working

--- differentialEquation.jl
+++ differentialEquation.jl
@@ -7,41 +7,47 @@ begin
     using SymPy
 end
 
-function B_f(v, dv, w, dw)
-    integral, err = quadgk((x) -> dv(x)*dw(x), 0, 3, rtol = 1e-3)
-    return integral
+function B_f(dv, i, dw, j, n)
+    if abs(i-j) > 1
+        return 0
+    end
+    h = 2/(n-1)
+    integral, err = quadgk((x) -> dv(x)*dw(x), min(0,h*(i-1)), min(3,h*(i+1)), rtol = 1e-3)
+    return -integral
 end
 
-function L_f(v,dv)
+function L_f(v)
     G = 6.67*10^-11
     integral1, err1 = quadgk(v, 1, 2, rtol = 1e-3)
-    integral2, err2 = quadgk(dv, 0, 3, rtol = 1e-3)
-    return -4*pi*G*integral1 + (1/3)*integral2
+    # integral2, err2 = quadgk(dv, 0, 3, rtol = 1e-3)
+    return 4*pi*G*integral1# + (1/3)*integral2
 end
 
 function solve_equatacion(n)
     println("-----")
-    e = [(x) -> (3*(i-1)/n <= x < 3*i/n) ? (n/3)*x - i + 1 :
-                (3*(i)/n <= x <= 3*(i+1)/n) ? 1 + i - (n/3)*x :
+    h = 2/(n-1)
+    e = [(x) -> (h*(i-1) <= x < h*i) ? x/h - i + 1 :
+                (h*i <= x <= h*(i+1)) ? 1 + i - x/h :
                 0
-                for i = 0:n]
-    de = [(x) -> (3*(i-1)/n <= x <= 3*i/n) ? n/3 :
-                 (3*(i)/n <= x <= 3*(i+1)/n) ? -n/3 :
+                for i = 1:n-1]
+    de = [(x) -> (h*(i-1) <= x < h*i) ? 1/h :
+                (h*i <= x <= h*(i+1)) ? -1/h :
                  0
-                 for i = 0:n]
+                 for i = 1:n-1]
 
-    A = [ B_f(e[i],de[i],e[j],de[j]) for i = 1:n-2, j = 1:n-2 ]
-    R = [ L_f(e[i],de[i]) + B_f(x -> 5 - x/3,x -> -1/3, e[i], de[i]) for  j = 1:1, i = 1:n-2 ]
+    A = [ B_f(de[j],j, de[i],i,n) for i = 1:n-1, j = 1:n-1 ]
+    R = [ L_f(e[i]) - B_f(x -> -1/3,i, de[i],i,n) for  j = 1:1, i = 1:n-1 ]
+    print("det(A) = ")
     println(det(A))
+    println("A ")
+    display(A)
     X = R/A
-    print("A ")
-    println(A)
-    println(det(A))
+
     print("R ")
     println(R)
     print("X")
     println(X)
-    Y = [sum([X[i]*e[i](j*3/10) for i in 1:n+1])  for j = 0:9]
+    Y = [sum([X[i]*e[i](j*3/10) for i in 1:n-1])  for j = 0:9]
     u = map((x) -> 5 - x/3, [(j*3/10) for j in 0:9])
     println(u)
     println("Y")
@@ -51,4 +57,4 @@ function solve_equatacion(n)
 
 end
 
-solve_equatacion(30)
+solve_equatacion(100)
